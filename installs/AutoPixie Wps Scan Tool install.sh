729caebdcda9334db005659e62e4d55387a151a8
NateWeiler <nateweiler84@gmail.com>
2020-07-13 14:08:12 -0400

    Add

--- installs/AutoPixie Wps Scan Tool install.sh
+++ installs/AutoPixie Wps Scan Tool install.sh
@@ -1,12 +1,38 @@
-#!/system/bin/sh
+# AUTOPIXIE TOOL
+echo Autopixie-WPS = this script is meant for people
 
-apt update && apt updrade -u
+echo who wants to check if someone can gain the wpa key,
+
+echo and or if you are protected from this attack Any illegal
+
+echo use of this program is strictly forbidden!.
+
+echo FEATURES
+
+echo Kill reaver as soon as e-hash2 is gained.
+
+echo Manual input target router without scan
+
+echo Wash scan > target router from scan list
+
+echo Save resuslts to logfile
+
+echo option to ignore router from wash scan if it has been cracked,
+
+echo or if PixieWps failed to crack the hash
+
+echo remember you must have an ecternal wifi adpter
+
+echo Installation :
+
+apt update && apt upgrade -u
 apt install git
 apt install python
-apt install python3
-pip3 install requests
-echo How to use AutoPixie Wps Scan Tool in Termux
-git clone https://github.com/nxxxu/AutoPixieWps.git
-AutoPixieWps
-chmod +x*
+pip install requests
+git clone https://github.com/nxxxu/AutoPixieWps
+cd AutoPixieWps
+chmod +x *
+
+echo Run :
+
 python3 autopixie.py
